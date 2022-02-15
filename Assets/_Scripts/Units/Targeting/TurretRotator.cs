cd904ad172ade52979b906315c6ecc63f6a200ea
o-fagerstam <55053302+o-fagerstam@users.noreply.github.com>
2022-02-15 17:09:30 +0100

    Turret rotation bug fixes + ParrelSync

--- Assets/_Scripts/Units/Targeting/TurretRotator.cs
+++ Assets/_Scripts/Units/Targeting/TurretRotator.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using Mirror;
+﻿using Mirror;
 using Sirenix.OdinInspector;
 using UnityEngine;
 namespace Units.Targeting {
-	public class TurretRotator : MonoBehaviour {
+	public class TurretRotator : NetworkBehaviour {
 		[SerializeField][Required][ChildGameObjectsOnly]
 		private Transform turretPivot;
 		[SerializeField][Required][ChildGameObjectsOnly]
 		private Targeter targeter;
-
-		[Server]
+		
+		[ServerCallback]
 		private void Update () {
+			if (!isServer) { return; }
 			if (targeter.Target != null) {
 				turretPivot.LookAt(targeter.Target.transform, Vector3.up);
 			}
